4dd1ad8e9d62b9687c3e538a455ef107608bbdf1
Phuwish <163742173+phuwishpk@users.noreply.github.com>
2024-11-19 21:40:48 +0700

    Update multiplication_table.py

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 def main():
-    # Prompt the user for a number
     number = int(input("Enter a number:\n"))
 
-    # Display the multiplication table for the given number
     for i in range(10):
         print(f"{i} x {number} = {i * number}")
 
