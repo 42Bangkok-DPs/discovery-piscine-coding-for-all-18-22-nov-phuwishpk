cf0df1f73be225f93043eb1be7fa3d218b27de23
Phuwish <163742173+phuwishpk@users.noreply.github.com>
2024-11-20 10:47:44 +0700

    Update advance_mult.py

--- cell03/ex03/advance_mult.py
+++ cell03/ex03/advance_mult.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-def main():
-    for i in range(11):
-        table = [i * j for j in range(11)]
-        print(f"Table de {i}: {' '.join(map(str, table))}")
 
-if __name__ == "__main__":
-    main()
+import sys
+if len(sys.argv) > 1:
+    if sys.argv[1] == "yolo":
+        print("none")
+        sys.exit()
+
+def generate_multiplication_tables():
+    for i in range(11):
+        print(f"Table de {i}: ", end="")
+        for j in range(11):
+            print(f"{i * j:2}", end=" ")
+        print()
+
+generate_multiplication_tables()
